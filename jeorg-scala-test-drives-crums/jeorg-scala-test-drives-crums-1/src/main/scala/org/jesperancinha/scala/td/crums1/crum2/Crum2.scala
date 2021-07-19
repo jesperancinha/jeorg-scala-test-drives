11fbf33337f87855bd1ea0e0db947cc2e37390b3
Joao Esperancinha <jofisaes@gmail.com>
2021-07-19 17:35:25 +0200

    📕 jeorg-scala-test-drives-crums-1 - Crum 2 - operators and code blocks

--- jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/crum2/Crum2.scala
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/crum2/Crum2.scala
@@ -2,11 +2,24 @@ package org.jesperancinha.scala.td.crums1.crum2
 
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 
-case object Crum2{
-  def main(args: Array[String]): Unit ={
+case object Crum2 {
+  def main(args: Array[String]): Unit = {
     ConsolerizerComposer.outSpace()
-      .cyan(ConsolerizerComposer.title("Crum 2 -operators"))
+      .cyan(ConsolerizerComposer.title("Crum 2 - operators and code blocks"))
 
+    val village = "people"
+    val total = if (village == "people") 1000 else 0
+    val totalDistance = {
+      val distanceFromNYToAustin = 2800
+      val distanceFromAustoToSanFrancico = 2820
+      distanceFromNYToAustin + distanceFromAustoToSanFrancico
+    }
+
+    ConsolerizerComposer.outSpace().none()
+      .cyan("We are going to see the village").green(village).newLine()
+      .cyan("Because of that, it costs").green(total.toString).cyan("dollars!").newLine()
+      .cyan("We drive from New York, making a stop in Austin, a distance of").green(totalDistance.toString).cyan("kilometers!").newLine()
+      .cyan("Made using the help of https://www.distance.to/").newLine().reset()
 
   }
 }
