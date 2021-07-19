df0a52e1d567756fde29416015b328f0bb2972a0
Joao Esperancinha <jofisaes@gmail.com>
2021-07-19 21:50:28 +0200

    📕 jeorg-scala-test-drives-crums-1 - Crum 3 - multiple constuctors

--- jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/Crums1.scala
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/Crums1.scala
@@ -4,6 +4,7 @@ import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer.title
 import org.jesperancinha.scala.td.crums1.crum1.Crum1
 import org.jesperancinha.scala.td.crums1.crum2.Crum2
+import org.jesperancinha.scala.td.crums1.crum3.Crum3
 
 object Crums1 {
   def main(args: Array[String]): Unit = {
@@ -19,5 +20,6 @@ object Crums1 {
 
     Crum1.main(args)
     Crum2.main(args)
+    Crum3.main(args)
   }
 }
---
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/crum3/Crum3.scala
@@ -0,0 +1,29 @@
+package org.jesperancinha.scala.td.crums1.crum3
+
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+
+object Crum3 {
+
+  def main(args: Array[String]): Unit = {
+    ConsolerizerComposer.outSpace()
+      .cyan(ConsolerizerComposer.title("Crum 3 - multiple constuctors"))
+
+    val emerald = Jewel("Emerald", 20)
+    val amethist = new Jewel("Amethist")
+    val diamond = new Jewel()
+    ConsolerizerComposer.outSpace().none()
+      .magenta("This is a stone").brightGreen(emerald).magenta(", we have").brightGreen(emerald.quantity.toString).magenta("of them.").newLine()
+      .magenta("This is another stone").brightMagenta(amethist).magenta(", we have").brightMagenta(amethist.quantity.toString).magenta("of them.").newLine()
+      .magenta("And this is the big one!").brightWhite(diamond).magenta(", we have").brightWhite(diamond.quantity.toString).magenta("of them.").newLine()
+  }
+
+  case class Jewel(val stone: String, val quantity: Long) {
+    def this(stone: String) {
+      this(stone, 10L)
+    }
+
+    def this() {
+      this("Diamond, 10L")
+    }
+  }
+}
