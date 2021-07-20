9144d80dee9455f9f6e8ead34c14863838b9666b
Joao Esperancinha <jofisaes@gmail.com>
2021-07-20 11:01:05 +0200

    📕 jeorg-scala-test-drives-crums-1 - Crum 10 - Multiple element tuples

--- jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/crum10/Crum10.scala
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-1/src/main/scala/org/jesperancinha/scala/td/crums1/crum10/Crum10.scala
@@ -3,12 +3,12 @@ package org.jesperancinha.scala.td.crums1.crum10
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 
 object Crum10 {
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     ConsolerizerComposer.outSpace()
       .cyan(ConsolerizerComposer.title("Crum 10 - Multiple element tuples"))
       .reset()
 
-    val tuple = Tuple10("red", "green", "blue", "purple","green","black","yellow","white","pink","cyan")
+    val tuple = Tuple10("red", "green", "blue", "purple", "green", "black", "yellow", "white", "pink", "cyan")
 
     ConsolerizerComposer.outSpace()
       .magenta(tuple)
@@ -19,6 +19,10 @@ object Crum10 {
 
     ConsolerizerComposer.outSpace()
       .cyan("It can also be done this way")
-      .brightCyan(("red", "green", "blue", "purple","green","black","yellow","white","pink","cyan"))
+      .brightCyan(("red", "green", "blue", "purple", "green", "black", "yellow", "white", "pink", "cyan"))
+      .cyan("or we can make one include the other")
+      .brightCyan(("red" -> "green" -> "blue" -> "purple" -> "green" -> "black" -> "yellow" -> "white" -> "pink" -> "cyan"))
+      .reset()
+
   }
 }
