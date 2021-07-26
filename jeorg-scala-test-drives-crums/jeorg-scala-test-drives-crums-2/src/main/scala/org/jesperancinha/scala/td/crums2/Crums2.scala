116859fde775b0847168c391224c86120a822f5f
Joao Esperancinha <jofisaes@gmail.com>
2021-07-26 16:12:11 +0200

    📕 jeorg-scala-test-drives-crums-2 - Crum 5 - Option, Some, None

--- jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-2/src/main/scala/org/jesperancinha/scala/td/crums2/Crums2.scala
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-2/src/main/scala/org/jesperancinha/scala/td/crums2/Crums2.scala
@@ -4,6 +4,7 @@ import org.jesperancinha.scala.td.crums2.crum1.Crum1
 import org.jesperancinha.scala.td.crums2.crum2.Crum2
 import org.jesperancinha.scala.td.crums2.crum3.Crum3
 import org.jesperancinha.scala.td.crums2.crum4.Crum4
+import org.jesperancinha.scala.td.crums2.crum5.Crum5
 
 object Crums2 {
 
@@ -12,6 +13,7 @@ object Crums2 {
     Crum2.main(args)
     Crum3.main(args)
     Crum4.main(args)
+    Crum5.main(args)
   }
 
 }
---
+++ jeorg-scala-test-drives-crums/jeorg-scala-test-drives-crums-2/src/main/scala/org/jesperancinha/scala/td/crums2/crum5/Crum5.scala
@@ -0,0 +1,37 @@
+package org.jesperancinha.scala.td.crums2.crum5
+
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+
+object Crum5 {
+  def main(args: Array[String]): Unit = {
+    ConsolerizerComposer.outSpace()
+      .cyan(ConsolerizerComposer.title("Crum 5 - Option, Some, None"))
+      .reset()
+
+    val partyChecker = (party: Option[String]) => {
+      party match {
+        case Some(p) =>
+          ConsolerizerComposer.outSpace().cyan("This party is").blue(p).reset()
+          party match {
+            case Some(p) if p == "It's my party"=> ConsolerizerComposer.outSpace().brightMagenta("MINE!")
+            case Some(p) if p == "It's their party"=> ConsolerizerComposer.outSpace().brightMagenta("THEIRS!")
+            case _ => ConsolerizerComposer.outSpace().brightRed("I don't know this!")
+          }
+        case None => ConsolerizerComposer.outSpace().cyan("No party").reset()
+      }
+    }
+
+    val myParty = "It's my party"
+
+    val optionMyParty = Option(myParty)
+
+    val theirParty = "It's their party"
+
+    val optionTheirParty = Option(theirParty)
+
+    partyChecker(optionMyParty)
+    partyChecker(optionTheirParty)
+    partyChecker(None)
+    partyChecker(Option("Whasup!!"))
+  }
+}
